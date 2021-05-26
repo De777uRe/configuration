136e3136e53127728c8d632cf19fa3156f489efb
de777ure <de777urest@gmail.com>
2021-05-26 13:37:28 -0600

    Add i3 configuration

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-declare -a files=(
+declare -a main_files=(
     ".bash_aliases"
     ".bashrc"
     ".functions"
@@ -8,8 +8,26 @@ declare -a files=(
     ".vimrc"
 )
 
-for f in ${files[@]}; do
+for f in ${main_files[@]}; do
     echo "Creating link for: $f"
     rm -f ~/$f
     ln -s ~/configuration/$f ~/$f
+done
+
+declare -a i3_files=(
+    "config"
+    "status"
+)
+
+for f in ${i3_files[@]}; do
+    if [[ "$f" == "config" ]]
+    then
+        echo "Creating configuration link for: $f"
+        rm -f ~/.config/i3/$f
+        ln -s ~/configuration/i3$f ~/.config/i3/$f
+    else
+        echo "Creating status link for: $f"
+        rm -f ~/.config/i3status/config
+        ln -s ~/configuration/i3$f ~/.config/i3status/config
+    fi
 done
